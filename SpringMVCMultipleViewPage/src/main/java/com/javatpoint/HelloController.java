1ff9a1eddf160bd66b3ceecad81a3ee0cceb0430
USER <sojib.19991018@gmail.com>
2020-07-21 23:36:38 +0600

    @Request Mapping Annotation in Spring MVC Full Game

--- SpringMVCMultipleViewPage/src/main/java/com/javatpoint/HelloController.java
+++ SpringMVCMultipleViewPage/src/main/java/com/javatpoint/HelloController.java
@@ -7,11 +7,13 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/first")
 public class HelloController {
-	@RequestMapping("/home")
+	@RequestMapping(path = "/home", method = RequestMethod.GET)
 	public String home(Model model) {
 		model.addAttribute("name", "Md. Sojib");
 		model.addAttribute("id", 11294);
